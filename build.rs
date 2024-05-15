a8974499a2a8c53535e86248ba78a4f4742939be
Zhiyao Ma <zhiyao.ma.98@gmail.com>
2024-05-15 23:53:43 +0800

    Add build script.

---
+++ build.rs
@@ -0,0 +1,14 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+
+fn main() {
+    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let link_x = include_bytes!("link.ld.in");
+
+    let mut f = File::create(out.join("link.ld")).unwrap();
+    f.write_all(link_x).unwrap();
+
+    println!("cargo:rustc-link-search={}", out.display());
+}
