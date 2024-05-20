c1aacc013ce04cf8a5adc1eb027b0ec68c5977e8
Zhiyao Ma <zhiyao.ma.98@gmail.com>
2024-05-21 01:08:14 +0800

    Add task sleeping example.

---
+++ examples/sleep.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+use hopter::{boot::main, debug::semihosting, hprintln, time};
+
+// Attribute `#[main]` marks the function as the entry function for the main
+// task. The function name can be arbitrary. The main function should accept
+// one argument which is the Cortex-M core peripherals.
+#[main]
+fn main(_: cortex_m::Peripherals) {
+    for _ in 0..5 {
+        // Let the task sleep for some time.
+        time::sleep_ms(1000);
+        // Print via semihosting. When using QEMU with semihosting option enabled,
+        // the characters will appear on the QEMU console.
+        hprintln!("hello");
+    }
+
+    // When running with QEMU, this will cause the QEMU process to terminate.
+    // Do not include this line when running with OpenOCD, because it will
+    // clobber its internal states.
+    semihosting::terminate(true);
+}
