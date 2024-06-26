122e1e6e6fbb7c3f81b5b5f7f13f04da97ec67d1
Ayak32 <112664452+Ayak32@users.noreply.github.com>
2024-06-26 13:05:16 -0400

    Update semaphore_test1.rs

--- examples/semaphore_test1.rs
+++ examples/semaphore_test1.rs
@@ -4,7 +4,7 @@
 extern crate alloc;
 use hopter::{boot::main, debug::semihosting, hprintln, time, sync::semaphores};
 
-static sema = semaphores::new(3,3)
+static SEMA = sema = semaphores::new(3,3)
 
 #[main]
 fn main(_: cortex_m::Peripherals) {
