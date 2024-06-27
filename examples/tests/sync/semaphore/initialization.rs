28fbca7dfa657e0024f6208bdd1a71f94a75a9a4
Ayak32 <112664452+Ayak32@users.noreply.github.com>
2024-06-27 12:01:02 -0400

    Update initialization.rs

--- examples/tests/sync/semaphore/initialization.rs
+++ examples/tests/sync/semaphore/initialization.rs
@@ -7,14 +7,14 @@ use hopter::{boot::main, debug::semihosting, hprintln, sync::Semaphore};
 #[main]
 fn main(_: cortex_m::Peripherals) {
   for i in 1..5 {
-        let semaphore = semaphores::new(i, i);
+        let semaphore = Semaphore::new(i, i);
     
-        if semaphore.count != i {
+        if Semaphore.count != i {
             hprintln!("Count Not Initialized Properly");
             semihosting::terminate(false);
         }
       
-        if semaphore.max_count != i {
+        if Semaphore.max_count != i {
             hprintln!("Max Count Not Initialized Properly");
             semihosting::terminate(false);
         }
