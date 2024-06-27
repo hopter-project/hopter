2b1bd92cd431c3cea1b62b3abbf6cf94e9908a68
Ayak32 <112664452+Ayak32@users.noreply.github.com>
2024-06-27 12:12:43 -0400

    Update initialization.rs

--- examples/tests/sync/semaphore/initialization.rs
+++ examples/tests/sync/semaphore/initialization.rs
@@ -1,27 +1,62 @@
+// #![no_std]
+// #![no_main]
+
+// extern crate alloc;
+// use hopter::{boot::main, debug::semihosting, hprintln, sync::Semaphore};
+
+// #[main]
+// fn main(_: cortex_m::Peripherals) {
+//   for i in 1..5 {
+//         let semaphore = Semaphore::new(i, i);
+    
+//         if semaphore.count != i {
+//             hprintln!("Count Not Initialized Properly");
+//             semihosting::terminate(false);
+//         }
+      
+//         if semaphore.max_count != i {
+//             hprintln!("Max Count Not Initialized Properly");
+//             semihosting::terminate(false);
+//         }
+// }
+
+
+//   hprintln!("Test Passed");
+//   semihosting::terminate(true);
+
+// }
+
 #![no_std]
 #![no_main]
 
 extern crate alloc;
 use hopter::{boot::main, debug::semihosting, hprintln, sync::Semaphore};
 
+
 #[main]
 fn main(_: cortex_m::Peripherals) {
-  for i in 1..5 {
-        let semaphore = Semaphore::new(i, i);
-    
-        if semaphore.count != i {
-            hprintln!("Count Not Initialized Properly");
-            semihosting::terminate(false);
-        }
-      
-        if semaphore.max_count != i {
-            hprintln!("Max Count Not Initialized Properly");
-            semihosting::terminate(false);
-        }
-}
+  let sema = semaphore::new(3,3);
 
+  for i in 0..3 {
+    sema.down();
+  }
 
-  hprintln!("Test Passed");
-  semihosting::terminate(true);
+  if sema.count != 0 {
+      hprintln!("Incorrect Count");
+      semihosting::terminate(false);
+  }
 
-}
+  let result = sema.try_down_allow_isr();
+
+  match result {
+      Ok(()) => {
+        hprintln!("Decremented at 0");
+        semihosting::terminate(false);
+      },
+      Err(()) => {
+        hprintln!("Test Passed"); 
+        semihosting::terminate(true);
+      }
+   }
+
+} 
