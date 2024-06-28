15a2f67762b99f31ee8f8e77b8f1d6a52030d08d
Ayak32 <112664452+Ayak32@users.noreply.github.com>
2024-06-28 16:44:42 -0400

    Update new_test_draft.rs

--- examples/tests/sync/semaphore/new_test_draft.rs
+++ examples/tests/sync/semaphore/new_test_draft.rs
@@ -2,19 +2,40 @@
 #![no_std]
 
 extern crate alloc;
-use hopter::{boot::main, debug::semihosting, hprintln, time, schedule, sync::Semaphore};
+use hopter::{boot::main, debug::semihosting, hprintln, schedule, sync::Semaphore};
 
-static SEMAPHORE: Semaphore = Semaphore::new(3,3);
+static SEMAPHORE: Semaphore = Semaphore::new(5,3);
 
 #[main]
 fn main(_: cortex_m::Peripherals) {
-  if SEMAPHORE.count == 3
-  {
-    semihosting::terminate(true);
-  }
-  else
-  {
-    semihosting::terminate(false);
-  }
+  let result = SEMAPHORE.try_down_allow_isr();
+
+  match result {
+      Ok(()) => continue,
+      Err(()) => 
+      {
+           hprintln!("Did not decremented");
+           semihosting::terminate(false);
+      };
+    }
+
+  // SEMAPHORE count should be 2
+    SEMAPHORE.down();
+    SEMAPHORE.down();
+
+ // SEMAPHORE count should be 0
+let second_result = SEMAPHORE.try_down_allow_isr();
+
+  match second_result {
+      Ok(()) => {
+           hprintln!("Decremented at 0");
+           semihosting::terminate(false);
+      },
+      Err(()) => 
+      {
+           hprintln!("Test Passed");
+           semihosting::terminate(true);
+      };
+    }
   
 }
