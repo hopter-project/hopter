d704cf7be909910a5c034473069905b62ae73a11
Ayak32 <112664452+Ayak32@users.noreply.github.com>
2024-07-01 16:07:48 -0400

    Update stress_test.rs

--- examples/tests/sync/semaphore/stress_test.rs
+++ examples/tests/sync/semaphore/stress_test.rs
@@ -6,7 +6,7 @@ use hopter::{boot::main, debug::semihosting, hprintln, schedule, sync::Semaphore
 
 #[main]
 fn main(_: cortex_m::Peripherals) {
-  for _ in 0..100{
+  for _ in 0..10{
     schedule::start_task(2, |_| task(), (), 0, 4).unwrap();
   }
   semihosting::terminate(true);
@@ -15,7 +15,7 @@ fn main(_: cortex_m::Peripherals) {
 static SEMAPHORE: Semaphore = Semaphore::new(10,5);
 
 fn task() {
-    for _ in 0..100 {
+    for _ in 0..10 {
       SEMAPHORE.up();
       SEMAPHORE.down();
   }
