13c99b34a4913a299e21f6c37926872267fd3abe
Ayak32 <112664452+Ayak32@users.noreply.github.com>
2024-07-01 12:54:14 -0400

    Create 2_tasks_5_down_5_up.rs

---
+++ examples/tests/sync/semaphore/2_tasks_5_down_5_up.rs
@@ -0,0 +1,33 @@
+#![no_main]
+#![no_std]
+
+extern crate alloc;
+use hopter::{boot::main, debug::semihosting, hprintln, time, schedule, sync::Semaphore};
+
+#[main]
+fn main(_: cortex_m::Peripherals) {
+  schedule::start_task(2, |_| task1(), (), 0, 4).unwrap();
+  schedule::start_task(2, |_| task2(), (), 0, 4).unwrap();
+}
+
+static SEMAPHORE: Semaphore = Semaphore::new(3,3);
+
+fn task1() {
+      hprintln!("Task 1 started");
+      for i in 0..5 {
+            SEMAPHORE.down();
+            hprintln!("Down {}", i);
+        }
+    hprintln!("Task2 completed");
+    semihosting::terminate(true);
+}
+
+fn task2() {
+      hprintln!("Task 2 started");
+      for i in 0..5 {
+            SEMAPHORE.up();
+            hprintln!("Up {}", i);
+        }
+    hprintln!("Task2 completed");
+    semihosting::terminate(true);
+  }
