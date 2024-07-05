987cda75a8fdd16e4f437e5d8b92dd434191207a
Ayak32 <112664452+Ayak32@users.noreply.github.com>
2024-07-05 15:00:48 -0400

    Create initialization.rs

---
+++ examples/tests/sync/semaphore/initialization.rs
@@ -0,0 +1,23 @@
+#![no_main]
+#![no_std]
+
+extern crate alloc;
+use hopter::{boot::main, debug::semihosting, schedule, hprintln, sync::Semaphore};
+
+
+#[main]
+fn main(_: cortex_m::Peripherals) {
+  for i in 0..5
+    {
+      for j in 5..10 {
+        let semaphore = Semaphore::new(j, i);
+        if semaphore.count() != i or semaphore.max_count() != j {
+            hprintln!("Test Failed");
+            semihosting::terminate(false);
+          }  
+      }
+    }
+    hprintln!("Test Passed");
+    semihosting::terminate(true);
+}
+
