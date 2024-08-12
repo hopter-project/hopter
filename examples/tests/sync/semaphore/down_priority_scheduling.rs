58a546460f7d2b85727b064fb0eb68efe43bc785
Zhiyao Ma <zhiyao.ma.98@gmail.com>
2024-08-12 16:13:13 -0400

    Add priority scheduling test for semaphore.

---
+++ examples/tests/sync/semaphore/down_priority_scheduling.rs
@@ -0,0 +1,53 @@
+#![no_main]
+#![no_std]
+
+extern crate alloc;
+use hopter::{boot::main, config, debug::semihosting, hprintln, sync::Semaphore, task};
+
+static SEMAPHORE: Semaphore = Semaphore::new(1, 0);
+
+#[main]
+fn main(_: cortex_m::Peripherals) {
+    // Create test tasks.
+    task::build()
+        .set_entry(low_task)
+        .set_priority(config::DEFAULT_TASK_PRIORITY + 1)
+        .spawn()
+        .unwrap();
+    task::build()
+        .set_entry(high_task)
+        .set_priority(config::DEFAULT_TASK_PRIORITY - 1)
+        .spawn()
+        .unwrap();
+    task::build()
+        .set_entry(middle_task)
+        .set_priority(config::DEFAULT_TASK_PRIORITY)
+        .spawn()
+        .unwrap();
+
+    // Let the test tasks run. But they will be blocked by the semaphore.
+    task::change_current_priority(config::DEFAULT_TASK_PRIORITY + 2).unwrap();
+
+    // Notify through the semaphore and the test tasks should be woken up based
+    // on their respective priority.
+    for _ in 0..3 {
+        SEMAPHORE.up();
+    }
+
+    semihosting::terminate(true);
+}
+
+fn high_task() {
+    SEMAPHORE.down();
+    hprintln!("High priority task executed");
+}
+
+fn middle_task() {
+    SEMAPHORE.down();
+    hprintln!("Middle priority task executed");
+}
+
+fn low_task() {
+    SEMAPHORE.down();
+    hprintln!("Low priority task executed");
+}
