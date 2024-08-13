d4a869194243738b30aa3c6fb36bb912ad912c71
Ayak32 <112664452+Ayak32@users.noreply.github.com>
2024-08-12 20:42:07 -0400

    Add tests for channel.

---
+++ examples/tests/sync/channel/multiple_producers.rs
@@ -0,0 +1,60 @@
+//! Tests behavior of a multi-producer, single-consumer channel
+//! It spawns two producer tasks with different priorities that add elements to a shared channel,
+//! and then consumes the elements in a specific order to verify the correct sequence
+
+#![no_main]
+#![no_std]
+
+extern crate alloc;
+use hopter::{boot::main, debug::semihosting, hprintln, sync, sync::Producer, task};
+
+#[main]
+fn main(_: cortex_m::Peripherals) {
+    // create a channel with a buffer capacity of 4
+    let (mut producer, consumer) = sync::create_channel::<usize, 4>();
+    // Clone the producer to allow multiple producers to push data into the channel
+    let mut producer2 = producer.clone();
+
+    // Spawn a task with the first producer
+    task::build()
+        .set_entry(move || produce_thread(&mut producer))
+        .set_priority(1)
+        .spawn()
+        .unwrap();
+    // Spawn a task with the second producer
+    task::build()
+        .set_entry(move || produce2_thread(&mut producer2))
+        .set_priority(2)
+        .spawn()
+        .unwrap();
+
+    // Change the priority of the current task to 10
+    // This allows the consumer to run after both producers have executed
+    task::change_current_priority(10).unwrap();
+
+    // Consume values from the channel and store them in a vector
+    let values = [
+        consumer.consume(),
+        consumer.consume(),
+        consumer.consume(),
+        consumer.consume(),
+    ];
+
+    // Verify the consumed values against the expected order
+    if values != [1, 2, 3, 4] {
+        hprintln!("Test Failed");
+        semihosting::terminate(false);
+    }
+    hprintln!("Test Passed");
+    semihosting::terminate(true);
+}
+
+fn produce_thread(producer: &mut Producer<usize, 4>) {
+    producer.produce(1);
+    producer.produce(2);
+}
+
+fn produce2_thread(producer2: &mut Producer<usize, 4>) {
+    producer2.produce(3);
+    producer2.produce(4);
+}
---
+++ examples/tests/sync/channel/try_consume.rs
@@ -0,0 +1,49 @@
+//! Test consume() and try_consume_allow_isr() functionality for empty and full channels
+#![no_main]
+#![no_std]
+
+extern crate alloc;
+use hopter::{boot::main, debug::semihosting, hprintln, sync};
+
+#[main]
+fn main(_: cortex_m::Peripherals) {
+    // Create a channel with a buffer capacity of 4 elements
+    let (producer, consumer) = sync::create_channel::<usize, 4>();
+
+    // Attempt to consume from an empty channel. Should return `None`.
+    let result = consumer.try_consume_allow_isr();
+
+    // Check against expected behavior
+    if result != None {
+        hprintln!("consumed from an empty channel");
+        semihosting::terminate(false);
+    }
+
+    // Fill channel to capacity with 4 elements
+    for i in 0..4 {
+        producer.produce(i);
+    }
+
+    // Empty channel by consuming iteratively, checking the element was consumed sucessfully each time
+    // otherwise, report error.
+    for _i in 0..4 {
+        let result = consumer.try_consume_allow_isr();
+        if result == None {
+            hprintln!("failed to consume from a non-empty channel");
+            semihosting::terminate(false);
+        }
+    }
+
+    // Attempt to consume from the empty channel. Should return `None`.
+    let final_result = consumer.try_consume_allow_isr();
+    match final_result {
+        None => {
+            hprintln!("Test Passed");
+            semihosting::terminate(true);
+        }
+        Some(_t) => {
+            hprintln!("consumed from an empty channel");
+            semihosting::terminate(false);
+        }
+    }
+}
