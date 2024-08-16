7eae4ccd06664ee0c30400d6bc6c62d2361eccc2
Zhiyao Ma <zhiyao.ma.98@gmail.com>
2024-08-16 13:05:25 -0400

    Add test for unwinding in interrupt handler.

---
+++ examples/tests/interrupt/unwind/simple.rs
@@ -0,0 +1,77 @@
+//!
+
+#![no_main]
+#![no_std]
+#![feature(naked_functions)]
+
+extern crate alloc;
+
+use core::sync::atomic::{AtomicUsize, Ordering};
+use hopter::{
+    boot::main,
+    debug::semihosting,
+    hprintln,
+    interrupt::handler,
+    sync::{AllIrqExceptSvc, MutexIrqSafe},
+};
+use stm32f4xx_hal::{
+    pac::{Interrupt, Peripherals, TIM2},
+    prelude::*,
+    timer::{CounterUs, Event},
+};
+
+static TIMER: MutexIrqSafe<Option<CounterUs<TIM2>>, AllIrqExceptSvc> = MutexIrqSafe::new(None);
+
+#[main]
+fn main(_cp: cortex_m::Peripherals) {
+    let dp = Peripherals::take().unwrap();
+
+    // For unknown reason QEMU accepts only the following clock frequency.
+    let rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.sysclk(16.MHz()).pclk1(8.MHz()).freeze();
+
+    let mut timer = dp.TIM2.counter(&clocks);
+
+    // Generate an interrupt when the timer expires.
+    timer.listen(Event::Update);
+
+    // Enable TIM2 interrupt.
+    unsafe {
+        cortex_m::peripheral::NVIC::unmask(Interrupt::TIM2);
+    }
+
+    // Set the timer to expire every 1 second.
+    // Empirically when set to 62 seconds the interval is actually
+    // approximately 1 second. Weird QEMU.
+    timer.start(62.secs()).unwrap();
+
+    // Move the timer into the global storage to prevent it from being dropped.
+    *TIMER.lock() = Some(timer);
+}
+
+/// Get invoked approximately every 1 second.
+#[handler(TIM2)]
+extern "C" fn tim2_handler() {
+    static IRQ_CNT: AtomicUsize = AtomicUsize::new(0);
+    let prev_cnt = IRQ_CNT.fetch_add(1, Ordering::SeqCst);
+
+    let _print_on_drop = PrintOnDrop("Resource released");
+
+    if prev_cnt % 2 == 0 {
+        panic!();
+    }
+
+    hprintln!("TIM2 IRQ count {}", prev_cnt);
+
+    if prev_cnt >= 5 {
+        semihosting::terminate(true);
+    }
+}
+
+struct PrintOnDrop(&'static str);
+
+impl Drop for PrintOnDrop {
+    fn drop(&mut self) {
+        hprintln!("{}", self.0);
+    }
+}
