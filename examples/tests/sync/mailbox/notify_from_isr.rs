f89822f167508e6048d44f9c448d9f663d877e2b
Ayak32 <112664452+Ayak32@users.noreply.github.com>
2024-08-21 17:00:28 -0400

    Test semaphore and mailbox in ISR context.

---
+++ examples/tests/sync/mailbox/notify_from_isr.rs
@@ -0,0 +1,90 @@
+//! Tests notifying a task from an ISR with `notify_allow_isr`.
+
+#![no_main]
+#![no_std]
+#![feature(naked_functions)]
+
+extern crate alloc;
+
+use core::sync::atomic::{AtomicUsize, Ordering};
+use hopter::{
+    boot::main,
+    config,
+    debug::semihosting,
+    hprintln,
+    interrupt::handler,
+    sync::{AllIrqExceptSvc, Mailbox, MutexIrqSafe},
+    task,
+};
+use stm32f4xx_hal::{
+    pac::{Interrupt, Peripherals, TIM2},
+    prelude::*,
+    timer::{CounterUs, Event},
+};
+
+static TIMER: MutexIrqSafe<Option<CounterUs<TIM2>>, AllIrqExceptSvc> = MutexIrqSafe::new(None);
+
+static MAILBOX: Mailbox = Mailbox::new();
+
+#[main]
+fn main(_cp: cortex_m::Peripherals) {
+    // Allow the new task below to run first until it blocks.
+    task::change_current_priority(config::DEFAULT_TASK_PRIORITY).unwrap();
+
+    // The new task should block on the semaphore.
+    task::build()
+        .set_entry(listener_function)
+        .set_priority(config::DEFAULT_TASK_PRIORITY - 1)
+        .spawn()
+        .unwrap();
+
+    let dp = Peripherals::take().unwrap();
+
+    // For unknown reason QEMU accepts only the following clock frequency.
+    let rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.sysclk(16.MHz()).pclk1(8.MHz()).freeze();
+
+    let mut timer = dp.TIM2.counter(&clocks);
+
+    // Generate an interrupt when the timer expires.
+    timer.listen(Event::Update);
+
+    // Enable TIM2 interrupt.
+    unsafe {
+        cortex_m::peripheral::NVIC::unmask(Interrupt::TIM2);
+    }
+
+    // Set the timer to expire every 1 second.
+    // Empirically when set to 62 seconds the interval is actually
+    // approximately 1 second. Weird QEMU.
+    timer.start(62.secs()).unwrap();
+
+    // Move the timer into the global storage to prevent it from being dropped.
+    *TIMER.lock() = Some(timer);
+}
+
+fn listener_function() {
+    for _ in 0..3 {
+        hprintln!("Waiting");
+        MAILBOX.wait();
+        hprintln!("Recieved");
+    }
+    semihosting::terminate(true);
+}
+
+/// Get invoked approximately every 1 second.
+#[handler(TIM2)]
+extern "C" fn tim2_handler() {
+    static IRQ_CNT: AtomicUsize = AtomicUsize::new(0);
+
+    MAILBOX.notify_allow_isr();
+    hprintln!("Notified");
+
+    let prev_cnt = IRQ_CNT.fetch_add(1, Ordering::SeqCst);
+
+    // If `prev_cnt`` is greater than 2, the program is stuck and should be
+    // terminated.
+    if prev_cnt > 2 {
+        semihosting::terminate(false);
+    }
+}
---
+++ examples/tests/sync/semaphore/try_up_from_isr.rs
@@ -0,0 +1,98 @@
+//! Tests notifying a task from an ISR with `try_up_allow_isr`.
+
+#![no_main]
+#![no_std]
+#![feature(naked_functions)]
+
+extern crate alloc;
+
+use core::sync::atomic::{AtomicUsize, Ordering};
+use hopter::{
+    boot::main,
+    config,
+    debug::semihosting,
+    hprintln,
+    interrupt::handler,
+    sync::{AllIrqExceptSvc, MutexIrqSafe, Semaphore},
+    task,
+};
+use stm32f4xx_hal::{
+    pac::{Interrupt, Peripherals, TIM2},
+    prelude::*,
+    timer::{CounterUs, Event},
+};
+
+static TIMER: MutexIrqSafe<Option<CounterUs<TIM2>>, AllIrqExceptSvc> = MutexIrqSafe::new(None);
+
+static SEMAPHORE: Semaphore = Semaphore::new(1, 0);
+
+#[main]
+fn main(_cp: cortex_m::Peripherals) {
+    // Allow the new task below to run first until it blocks.
+    task::change_current_priority(config::DEFAULT_TASK_PRIORITY).unwrap();
+
+    // The new task should block on the semaphore.
+    task::build()
+        .set_entry(down_function)
+        .set_priority(config::DEFAULT_TASK_PRIORITY - 1)
+        .spawn()
+        .unwrap();
+
+    let dp = Peripherals::take().unwrap();
+
+    // For unknown reason QEMU accepts only the following clock frequency.
+    let rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.sysclk(16.MHz()).pclk1(8.MHz()).freeze();
+
+    let mut timer = dp.TIM2.counter(&clocks);
+
+    // Generate an interrupt when the timer expires.
+    timer.listen(Event::Update);
+
+    // Enable TIM2 interrupt.
+    unsafe {
+        cortex_m::peripheral::NVIC::unmask(Interrupt::TIM2);
+    }
+
+    // Set the timer to expire every 1 second.
+    // Empirically when set to 62 seconds the interval is actually
+    // approximately 1 second. Weird QEMU.
+    timer.start(62.secs()).unwrap();
+
+    // Move the timer into the global storage to prevent it from being dropped.
+    *TIMER.lock() = Some(timer);
+}
+
+fn down_function() {
+    for _ in 0..3 {
+        hprintln!("Before task blocking");
+        SEMAPHORE.down();
+        hprintln!("After task resuming");
+    }
+    semihosting::terminate(true);
+}
+
+/// Get invoked approximately every 1 second.
+#[handler(TIM2)]
+extern "C" fn tim2_handler() {
+    // Only run this handler for three times. If running more than three times,
+    // the test task must have been stuck.
+    static COUNT: AtomicUsize = AtomicUsize::new(0);
+    if COUNT.fetch_add(1, Ordering::SeqCst) >= 3 {
+        semihosting::terminate(false);
+    }
+
+    // Attempt to `up` the semaphore. Assuming that the task can keep up with
+    // the interval of 1 second, which it just should unless QEMU is super
+    // broken.
+    let result = SEMAPHORE.try_up_allow_isr();
+    match result {
+        Ok(_) => {
+            hprintln!("Semaphore up to {}", SEMAPHORE.count())
+        }
+        Err(_) => {
+            hprintln!("Failed to up");
+            semihosting::terminate(false);
+        }
+    }
+}
