75faaaa7970e2281d284d9437ae48caf66f7b3f2
Zhiyao Ma <zhiyao.ma.98@gmail.com>
2024-08-26 01:02:37 -0400

    Refactor mutex implementation.

--- src/sync/mutex.rs
+++ src/sync/mutex.rs
@@ -2,7 +2,7 @@ use super::{
     GenericSpin, GenericSpinGuard, Holdable, Lockable, SpinSchedSafe, UnlockableGuard, WaitQueue,
 };
 use crate::{
-    interrupt::mask::HeldInterrupt,
+    interrupt::mask::{HeldInterrupt, RecursivelyMaskable},
     schedule::{
         current,
         scheduler::{SchedSuspendGuard, Scheduler},
@@ -16,33 +16,15 @@ use core::{
 };
 use owning_ref::StableAddress;
 
-/// A lock that will block the current task if it tries to acquire the lock
-/// when the lock is being held by other tasks, similar to `std::sync::Mutex`.
-pub type Mutex<T> = MutexGeneric<T, (), ()>;
-
-/// Similar to `std::sync::MutexGuard`.
-pub type MutexGuard<'a, T> = MutexGenericGuard<'a, T, (), ()>;
-
-/// A lock that will block the current task if it tries to acquire the lock
-/// when the lock is being held by other tasks, similar to `std::sync::Mutex`.
-/// When the lock is acquired, the corresponding interrupt will also be
-/// atomically masked.
-pub type MutexIrqSafe<T, I> = MutexGeneric<T, I, HeldInterrupt<I>>;
-
-/// Similar to `std::sync::MutexGuard`, but with an additional feature that
-/// while the guard is not dropped, the corresponding interrupt will continue
-/// to be masked.
-pub type MutexIrqSafeGuard<'a, T, I> = MutexGenericGuard<'a, T, I, HeldInterrupt<I>>;
-
-/// Generic type of a mutex. When the mutex is being held, some extra
-/// condition can also be held. For example, the extra condition may be
-/// some interrupt being masked. Failure to acquire a mutex will cause
-/// the calling task to block until the mutex can be acquired.
+/// Generic type of a mutex. When the mutex is acquired, some extra conditions
+/// can also be held. For example, an extra condition may be that some IRQs are
+/// being masked. Failure to acquire a mutex will cause the calling task to
+/// block until the mutex can be acquired.
 /// Generic parameter:
 /// - `T``: The type that the lock is protecting.
 /// - `H``: A holdable condition.
 /// - `G``: The guard type representing the additional held condition.
-pub struct MutexGeneric<T, H, G>
+pub struct GenericMutex<T, H, G>
 where
     T: ?Sized,
     H: Holdable<GuardType = G>,
@@ -67,22 +49,22 @@ where
 /// Generic type of a mutex guard that can dereference into contained type.
 /// When the guard is dropped, the mutex will be released and the additionally
 /// held condition will be released atomically.
-pub struct MutexGenericGuard<'a, T, H, G>
+pub struct GenericMutexGuard<'a, T, H, G>
 where
     T: ?Sized,
     H: Holdable<GuardType = G>,
 {
     guard: Option<GenericSpinGuard<'a, T, G>>,
-    mutex: &'a MutexGeneric<T, H, G>,
+    mutex: &'a GenericMutex<T, H, G>,
 }
 
-impl<T, H, G> MutexGeneric<T, H, G>
+impl<T, H, G> GenericMutex<T, H, G>
 where
     H: Holdable<GuardType = G>,
 {
     /// Create a new mutex instance.
     pub const fn new(data: T) -> Self {
-        MutexGeneric {
+        GenericMutex {
             queue: WaitQueue::new(),
             owner: SpinSchedSafe::new(None),
             poisoned: AtomicBool::new(false),
@@ -95,11 +77,16 @@ where
         self.spin_lock.into_inner()
     }
 
-    /// Return if the mutex was being held when an unwinding occurred.
+    /// Return if the mutex was acquired by a task when the task is being
+    /// unwound.
     pub fn is_poisoned(&self) -> bool {
         self.poisoned.load(Ordering::SeqCst)
     }
 
+    /// Forecefully set the mutex into released state.
+    ///
+    /// Safety: The previous acquired mutex guard must not be used to access
+    /// the protected data in the future.
     pub unsafe fn force_unlock(&self) {
         self.owner.lock_now_or_die().take();
         self.spin_lock.force_unlock();
@@ -107,14 +94,14 @@ where
     }
 }
 
-impl<T, H, G> MutexGeneric<T, H, G>
+impl<T, H, G> GenericMutex<T, H, G>
 where
     T: ?Sized,
     H: Holdable<GuardType = G>,
 {
     /// If the mutex has not been locked, lock it and return it within `Some`.
     /// Otherwise, return `None`.
-    pub fn try_lock(&self) -> Option<MutexGenericGuard<T, H, G>> {
+    pub fn try_lock(&self) -> Option<GenericMutexGuard<T, H, G>> {
         self.spin_lock
             // Try to acquire the spin lock that wraps around the protected data. If it
             // failes, i.e. returns `None`, the following will be skipped and this
@@ -145,8 +132,8 @@ where
                 // Discard the `.1` component which is the scheduler suspend guard.
                 _held: guard._held.0,
             })
-            // Finally, we convert it to `MutexGenericGuard`.
-            .map(|guard| MutexGenericGuard {
+            // Finally, we convert it to `GenericMutexGuard`.
+            .map(|guard| GenericMutexGuard {
                 guard: Some(guard),
                 mutex: &self,
             })
@@ -154,7 +141,7 @@ where
 
     /// Lock the mutex. If the mutex has already been locked, block until the
     /// current task can lock it.
-    pub fn lock(&self) -> MutexGenericGuard<T, H, G> {
+    pub fn lock(&self) -> GenericMutexGuard<T, H, G> {
         // Fast path for no contention.
         if let Some(guard) = self.try_lock() {
             return guard;
@@ -174,7 +161,7 @@ where
     }
 }
 
-impl<'a, T, H, G> Deref for MutexGenericGuard<'a, T, H, G>
+impl<'a, T, H, G> Deref for GenericMutexGuard<'a, T, H, G>
 where
     T: ?Sized,
     H: Holdable<GuardType = G>,
@@ -186,7 +173,7 @@ where
     }
 }
 
-impl<'a, T, H, G> DerefMut for MutexGenericGuard<'a, T, H, G>
+impl<'a, T, H, G> DerefMut for GenericMutexGuard<'a, T, H, G>
 where
     T: ?Sized,
     H: Holdable<GuardType = G>,
@@ -196,7 +183,7 @@ where
     }
 }
 
-impl<'a, T, H, G> Drop for MutexGenericGuard<'a, T, H, G>
+impl<'a, T, H, G> Drop for GenericMutexGuard<'a, T, H, G>
 where
     T: ?Sized,
     H: Holdable<GuardType = G>,
@@ -223,33 +210,184 @@ where
     }
 }
 
-impl<T, H, G> Lockable for MutexGeneric<T, H, G>
+impl<T, H, G> Lockable for GenericMutex<T, H, G>
 where
     T: ?Sized,
     H: Holdable<GuardType = G>,
 {
-    type GuardType<'a> = MutexGenericGuard<'a, T, H, G> where T: 'a, H: 'a, G:'a;
+    type GuardType<'a> = GenericMutexGuard<'a, T, H, G> where T: 'a, H: 'a, G:'a;
 
     fn lock_and_get_guard(&self) -> Self::GuardType<'_> {
         self.lock()
     }
 }
 
-impl<'a, T, H, G> UnlockableGuard<'a> for MutexGenericGuard<'a, T, H, G>
+impl<'a, T, H, G> UnlockableGuard<'a> for GenericMutexGuard<'a, T, H, G>
 where
     T: ?Sized,
     H: Holdable<GuardType = G>,
 {
-    type LockType = MutexGeneric<T, H, G>;
+    type LockType = GenericMutex<T, H, G>;
 
     fn unlock_and_into_lock_ref(self) -> &'a Self::LockType {
         self.mutex
     }
 }
 
-unsafe impl<'a, T, H, G> StableAddress for MutexGenericGuard<'a, T, H, G>
+unsafe impl<'a, T, H, G> StableAddress for GenericMutexGuard<'a, T, H, G>
 where
     T: ?Sized,
     H: Holdable<GuardType = G>,
 {
 }
+
+/// Generate the definition and methods and trait implementation of a mutex.
+/// The generated mutex wraps around [`GenericMutex`] and the generated mutex
+/// gruard wraps around [`GenericMutexGuard`]. The generated methods simply
+/// calls the identically named methods in the wrapped generic instance.
+///
+/// The reason for wrapping the generic mutex and gurad type is to prevent
+/// leaking internal types.
+///
+/// This macro also allows to generate different documentation for each
+/// concrete mutex type and guard type.
+macro_rules! define_mutex {
+    (
+        // The type name of the mutex struct to be generated.
+        $mutex_ty:ident,
+        // The documentation of the mutex struct in addition to the common
+        // documentation.
+        $mutex_extra_doc:expr,
+        // The type name of the mutex guard struct to be generated.
+        $guard_ty:ident,
+        // The documentation of the mutex guard struct in addition to the
+        // common documentation.
+        $guard_extra_doc:expr,
+        // Generic type parameters to the mutex type and their constraints.
+        <$($gen:ident $(: $bound:path)?),*>,
+        // Concrete type providing the implementation of the mutex.
+        $generic_mutex_ty:ty,
+        // Concrete type providing the implementation of the mutex gurad.
+        $generic_guard_ty:ty
+    ) => {
+        /// A lock type protecting the contained data. At most one task can
+        /// access the protected data at any time. Failure to acquire a mutex
+        /// will cause the calling task to block until the mutex can be
+        /// acquired.
+        ///
+        #[doc = $mutex_extra_doc]
+        pub struct $mutex_ty<$($gen $(: $bound)?),*> {
+            generic_mutex: $generic_mutex_ty,
+        }
+
+        /// The guard type that provides mutable access to the data being
+        /// protected. The guard will be returned after successfully acquiring
+        /// the mutex.
+        ///
+        #[doc = $guard_extra_doc]
+        pub struct $guard_ty<'a, $($gen $(: $bound)?),*> {
+            generic_guard: $generic_guard_ty,
+            mutex: &'a $mutex_ty<$($gen),*>,
+        }
+
+        impl<$($gen $(: $bound)?),*> $mutex_ty<$($gen),*> {
+            /// Create a new mutex instance protecting the given `data`.
+            pub const fn new(data: T) -> Self {
+                Self {
+                    generic_mutex: GenericMutex::new(data),
+                }
+            }
+
+            /// Discard the mutex and get back the contained data.
+            pub fn into_inner(self) -> T {
+                self.generic_mutex.into_inner()
+            }
+
+            /// Return if the mutex was acquired by a task when the task is
+            /// being unwound.
+            pub fn is_poisoned(&self) -> bool {
+                self.generic_mutex.is_poisoned()
+            }
+
+            /// Forecefully set the mutex into released state.
+            ///
+            /// Safety: The previous acquired mutex guard must not be used to
+            /// access the protected data in the future.
+            pub unsafe fn force_unlock(&self) {
+                self.generic_mutex.force_unlock()
+            }
+
+            /// If the mutex has not been locked, lock it and return it within
+            /// `Some`. Otherwise, return `None`.
+            pub fn try_lock<'a>(&'a self) -> Option<$guard_ty<'a, $($gen),*>> {
+                self.generic_mutex
+                    .try_lock()
+                    .map(|generic_guard| $guard_ty {
+                        generic_guard,
+                        mutex: self,
+                    })
+            }
+
+            /// Lock the mutex. If the mutex has already been locked, block
+            /// until the current task can lock it.
+            pub fn lock<'a>(&'a self) -> $guard_ty<'a, $($gen),*> {
+                $guard_ty {
+                    generic_guard: self.generic_mutex.lock(),
+                    mutex: self,
+                }
+            }
+        }
+
+        impl<'a, $($gen $(: $bound)?),*> Deref for $guard_ty<'a, $($gen),*> {
+            type Target = T;
+
+            fn deref(&self) -> &T {
+                self.generic_guard.deref()
+            }
+        }
+
+        impl<'a, $($gen $(: $bound)?),*> DerefMut for $guard_ty<'a, $($gen),*> {
+            fn deref_mut(&mut self) -> &mut T {
+                self.generic_guard.deref_mut()
+            }
+        }
+
+        impl<$($gen $(: $bound)?),*> Lockable for $mutex_ty<$($gen),*> {
+            type GuardType<'a> = $guard_ty<'a, $($gen),*> where $($gen:'a), *;
+
+            fn lock_and_get_guard(&self) -> Self::GuardType<'_> {
+                self.lock()
+            }
+        }
+
+        impl<'a, $($gen $(: $bound)?),*> UnlockableGuard<'a> for $guard_ty<'a, $($gen),*> {
+            type LockType = $mutex_ty<$($gen),*>;
+
+            fn unlock_and_into_lock_ref(self) -> &'a Self::LockType {
+                self.mutex
+            }
+        }
+
+        unsafe impl<'a, $($gen $(: $bound)?),*> StableAddress for $guard_ty<'a, $($gen),*> {}
+    };
+}
+
+define_mutex!(
+    Mutex,
+    "",
+    MutexGuard,
+    "",
+    <T>,
+    GenericMutex<T, (), ()>,
+    GenericMutexGuard<'a, T, (), ()>
+);
+
+define_mutex!(
+    MutexIrqSafe,
+    "When the mutex is acquired, the associated IRQ will also be masked.",
+    MutexIrqSafeGuard,
+    "The associated IRQ will be masked until the guard is dropped.",
+    <T, I: RecursivelyMaskable>,
+    GenericMutex<T, I, HeldInterrupt<I>>,
+    GenericMutexGuard<'a, T, I, HeldInterrupt<I>>
+);
