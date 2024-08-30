8a5aa34b1577f4a92404715b3c4a2ed91c134aa8
Ouhznehc <ouhznehc@foxmail.com>
2024-08-30 14:48:47 -0400

    change STM32F411 clock

--- examples/tests/debug/cpu_load/load_40_percent.rs
+++ examples/tests/debug/cpu_load/load_40_percent.rs
@@ -46,14 +46,14 @@ fn main(_cp: cortex_m::Peripherals) {
 
     // For unknown reason QEMU accepts only the following clock frequency.
     let rcc = dp.RCC.constrain();
-    // rcc.cfgr.sysclk(16.MHz()).pclk1(8.MHz()).freeze();
+
     #[cfg(feature = "stm32f411")]
     let clocks = rcc
         .cfgr
         .use_hse(8.MHz())
-        .sysclk(180.MHz())
-        .pclk1(45.MHz())
-        .pclk2(90.MHz())
+        .sysclk(100.MHz())
+        .pclk1(25.MHz())
+        .pclk2(50.MHz())
         .freeze();
     #[cfg(feature = "stm32f407")]
     let clocks = rcc
