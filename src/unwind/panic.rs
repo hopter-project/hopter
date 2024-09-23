95181d32cb27f18dc36680d5ac72b67c99f9f63f
Zhiyao Ma <zhiyao.ma.98@gmail.com>
2024-09-23 13:05:45 -0400

    Fix linker error when unwinding is disabled.

--- src/unwind/panic.rs
+++ src/unwind/panic.rs
@@ -5,3 +5,26 @@ use core::panic::PanicInfo;
 fn panic(_info: &PanicInfo) -> ! {
     unrecoverable::die();
 }
+
+/* Below are unused personality routines. They are marked unsafe because */
+/* they should not be invoked by any programmer's code. */
+
+#[no_mangle]
+unsafe extern "C" fn __aeabi_unwind_cpp_pr0() -> ! {
+    unrecoverable::die_with_arg("__aeabi_unwind_cpp_pr0: unexpectedly invoked.")
+}
+
+#[no_mangle]
+unsafe extern "C" fn __aeabi_unwind_cpp_pr1() -> ! {
+    unrecoverable::die_with_arg("__aeabi_unwind_cpp_pr1: unexpectedly invoked.")
+}
+
+#[no_mangle]
+unsafe extern "C" fn __aeabi_unwind_cpp_pr2() -> ! {
+    unrecoverable::die_with_arg("__aeabi_unwind_cpp_pr2: unexpectedly invoked.")
+}
+
+#[lang = "eh_personality"]
+unsafe extern "C" fn eh_personality() {
+    unrecoverable::die_with_arg("eh_personality: unexpectedly invoked.")
+}
